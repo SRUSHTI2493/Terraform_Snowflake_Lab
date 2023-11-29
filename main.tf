e7d92bc3660661f3da944791b3aad419d26f4cde
Srushti Pawar <87080882+SRUSHTI2493@users.noreply.github.com>
2023-11-29 11:24:47 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -29,22 +29,3 @@ resource "snowflake_schema" "demo_schema" {
   comment  = "Schema for Snowflake Terraform demo"
 }
 
-resource "snowflake_table" "demo_table" {
-  database = snowflake_database.demo_db.name
-  schema   = snowflake_schema.demo_schema.name
-  name     = "DEMO_TABLE"
-
-  column {
-    name     = "column1"
-    type     = "VARCHAR(255)"
-    comment  = "Description of column1"
-  }
-
-  column {
-    name     = "column2"
-    type     = "NUMBER"
-    comment  = "Description of column2"
-  }
-
-  comment  = "Table for Snowflake Terraform demo"
-}
