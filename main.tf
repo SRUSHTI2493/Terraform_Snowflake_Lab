d321bdc843b61fd2a59e6442e2998be1e2ac56dd
Srushti Pawar <87080882+SRUSHTI2493@users.noreply.github.com>
2023-11-29 12:14:44 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,19 +5,19 @@ terraform {
       version = "0.25.17"
     }
   }
-
+ 
   backend "remote" {
     organization = "Manchaft"
-
+ 
     workspaces {
       name = "gh-actions-demo"
     }
   }
 }
-
+ 
 provider "snowflake" {
 }
-
+ 
 resource "snowflake_database" "demo_db" {
   name    = "DEMO_DB"
   comment = "Database for Snowflake Terraform demo"
