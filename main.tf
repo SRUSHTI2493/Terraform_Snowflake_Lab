2185a32e00650a73023352176f15713f9c8e559d
Srushti Pawar <87080882+SRUSHTI2493@users.noreply.github.com>
2023-11-29 12:11:50 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,3 @@
-
 terraform {
   required_providers {
     snowflake = {
@@ -23,14 +22,11 @@ resource "snowflake_database" "demo_db" {
   name    = "DEMO_DB"
   comment = "Database for Snowflake Terraform demo"
 }
-
 resource "snowflake_schema" "demo_schema" {
   database = snowflake_database.demo_db.name
   name     = "DEMO_SCHEMA"
   comment  = "Schema for Snowflake Terraform demo"
 }
-
-
 resource "snowflake_table" "demo_table" {
   database = snowflake_database.demo_db.name
   schema   = snowflake_schema.demo_schema.name
